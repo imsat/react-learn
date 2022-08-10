c205445bdbe27e7e09e8a1f8283e00353907bb01
imsat <satyajit.cse@yahoo.com>
2022-08-10 19:13:34 +0600

    Update

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from './robots';
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+  render() {
+    return (
+      <div className="tc">
+        <h1 className="">RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    )
+  }
+  
 }
 
 export default App;
