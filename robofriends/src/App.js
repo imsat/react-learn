e2cdeed5e84072fc81d9fb986485e3c4e8816b92
imsat <satyajit.cse@yahoo.com>
2022-08-20 18:42:54 +0600

    Server request done

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -11,23 +11,41 @@ class App extends Component {
       robots: [],
       searchField: ''
     }
+    // console.log('constructor');
   }
+
+  componentDidMount() {
+    // console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+      this.setState({robots: users})
+    })
+  }
+
   onSearchChange = (e) =>{
     this.setState({searchField: e.target.value})
   }
 
   render() {
+    // console.log('render');
     const filterdRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     });
 
-    return (
-      <div className="tc">
-        <h1 className="logo f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterdRobots} />
-      </div>
-    )
+    if(this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    }else{
+      return (
+        <div className="tc">
+          <h1 className="logo f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filterdRobots} />
+        </div>
+      )
+    }
+
+    
   }
   
 }
