8744fe13dde7a711d545c2c065249f3422cb3512
imsat <satyajit.cse@yahoo.com>
2022-08-20 18:59:39 +0600

    Custom scroll component done

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import Scroll from "./Scroll"
 import { robots } from './robots';
 import './App.css'
 
@@ -40,7 +41,9 @@ class App extends Component {
         <div className="tc">
           <h1 className="logo f2">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filterdRobots} />
+          <Scroll>
+            <CardList robots={filterdRobots} />
+          </Scroll>
         </div>
       )
     }
