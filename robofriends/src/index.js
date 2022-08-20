df97e7602aad0fe9cd26d48009240df5c719e8cd
imsat <satyajit.cse@yahoo.com>
2022-08-20 22:10:32 +0600

    Redux middleware done

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import {searchRobots} from './reducers';
 
-const store = createStore(searchRobots)
+const logger  = createLogger() ;
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
@@ -22,4 +24,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); 
