5becae7e3d0366f5e0b50bbc788adb065329683b
imsat <satyajit.cse@yahoo.com>
2022-08-20 21:22:11 +0600

    Install react redux

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
