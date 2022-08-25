3b2637446f7aebcedeaabeebba8a5cf3d4eef405
imsat <satyajit.cse@yahoo.com>
2022-08-25 19:04:49 +0600

    Navbar done

---
+++ pokedex/src/containers/AppNavigator.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles }from '@material-ui/core'
+import { Pinterest } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+    AppBar: {
+        backgroundColor: 'black'
+    },
+    link: {
+        textDecoration: 'none'
+    },
+    title: {
+       cursor: 'pointer',
+       color: 'white'
+
+    }
+}))
+
+export default function AppNavigator() {
+    const classes = useStyles()
+
+  return (
+    <div>
+        <AppBar className={classes.AppBar} position='fixed' variant='h6'>
+            <Toolbar>
+                <Link to="/" className={classes.link}>
+                    <Typography className={classes.title}>Pokedex</Typography>
+                </Link> 
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
---
+++ pokedex/src/containers/Pokedex.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppNavigator from './AppNavigator'
+
+export default function Pokedex() {
+  return (
+    <div>
+        <AppNavigator />
+    </div>
+  )
+}
