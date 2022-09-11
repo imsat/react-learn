9c0eed3f3f93f273d2653ee595fd96e800533cd2
imsat <satyajit.cse@yahoo.com>
2022-09-12 01:16:12 +0600

    Basic jsx rules done

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,7 +10,45 @@ const root =  ReactDOM.createRoot(el)
 
 // 4) Create a component
 function App() {
-    return <h1>Hi there</h1>
+    // const date = new Date()
+    // const time = date.toLocaleTimeString()
+    // const name = 'Satyajit Mondal'
+    // const age = 23
+
+    // let message = {00-50
+    //     color: 'red'
+    // }
+
+    // if(Math.random() > 0.5) {
+    //     message = 'Hello there'
+    // }
+
+    // return <h1>
+    // { new Date().toLocaleTimeString() }
+    // </h1>
+
+    // const inputType = 'number'
+    // const minvalue = 5
+
+    // return <input type={inputType} min={minvalue}  />
+
+    // return <input style={{border: '3px solid red'}} type="number" min={0}  />
+
+
+    // return <textarea autoFocus={true} /> //Rule #1 Camel case 2 word attributes
+    // return <input style={{backgroundColor: 'red'}} maxLength={5} /> //Rule #2 for number use only curly braces
+
+     return (
+        <div className="rtr">
+            <textarea 
+            autoFocus={true} 
+            readOnly={false}
+            spellCheck={true}
+            style={{border: '5px solid brown'}}
+            />
+        </div>
+     )
+
 }
 
 // 5) Show the component on the screen
