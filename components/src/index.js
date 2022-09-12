4c2c903153d6316780b168847575373e4b5560f1
imsat <satyajit.cse@yahoo.com>
2022-09-13 01:59:21 +0600

    comment box design done

--- components/src/index.js
+++ components/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { faker } from '@faker-js/faker';
 
 const el = document.getElementById('root')
 
@@ -10,7 +11,39 @@ const App = () => {
         <div className='ui container comments'>
             <div className='comment'>
                 <a href='/'className='avatar'> 
-                    <img alt='avatar' />
+                    <img alt='avatar' src={faker.image.avatar()} />
+                </a>
+                <div className='content'>
+                    <a href='/'className='author' >
+                        Johon
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href='/'className='avatar'> 
+                    <img alt='avatar' src={faker.image.avatar()} />
+                </a>
+                <div className='content'>
+                    <a href='/'className='author' >
+                        Johon
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
+            <div className='comment'>
+                <a href='/'className='avatar'> 
+                    <img alt='avatar' src={faker.image.avatar()} />
                 </a>
                 <div className='content'>
                     <a href='/'className='author' >
