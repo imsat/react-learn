9ba3731ba7c0addde878609636800189025e0e97
imsat <satyajit.cse@yahoo.com>
2022-09-14 01:55:11 +0600

    Update

--- components/src/index.js
+++ components/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { faker } from '@faker-js/faker';
+import CommentDetail from './CommentDetail';
 
 const el = document.getElementById('root')
 
@@ -9,54 +9,9 @@ const root =  ReactDOM.createRoot(el)
 const App = () => {
     return (
         <div className='ui container comments'>
-            <div className='comment'>
-                <a href='/'className='avatar'> 
-                    <img alt='avatar' src={faker.image.avatar()} />
-                </a>
-                <div className='content'>
-                    <a href='/'className='author' >
-                        Johon
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
-            <div className='comment'>
-                <a href='/'className='avatar'> 
-                    <img alt='avatar' src={faker.image.avatar()} />
-                </a>
-                <div className='content'>
-                    <a href='/'className='author' >
-                        Johon
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
-            <div className='comment'>
-                <a href='/'className='avatar'> 
-                    <img alt='avatar' src={faker.image.avatar()} />
-                </a>
-                <div className='content'>
-                    <a href='/'className='author' >
-                        Johon
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!
-                    </div>
-                </div>
-            </div>
+            <CommentDetail author="Sam" />
+            <CommentDetail author="Alex" />
+            <CommentDetail author="Johonnnn" />
         </div>
     )
 }
