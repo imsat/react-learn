9a42a65ae753dc0355669dad37c9b13a07b1a7ee
imsat <satyajit.cse@yahoo.com>
2022-09-20 01:38:28 +0600

    Update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import SeasonDisplay from './SeasonDisplay'
 
 const el = document.getElementById('root')
 
 const root =  ReactDOM.createRoot(el)
 
-const App = () => {
-    return (
-        <div className="">Hi There</div>
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            lat: null,
+            lon: null,
+        }
+    }
+
+
+    render() {
+        // console.log('render')
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ 
+                    lat: position.coords.latitude, 
+                    lon: position.coords.longitude 
+                })
+            },
+            (error) => console.log(error),
+        );
+    
+        return <div>
+            Latitude: { this.state.lat }
+            <br />
+            Longitude : { this.state.lon }
+            </div>
+    }
 }
+
+
 
 root.render(<App />)
