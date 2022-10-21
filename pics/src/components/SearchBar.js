81c92b542c5dd328eabace96921736d653a1db17
imsat <satyajit.cse@yahoo.com>
2022-10-22 02:42:35 +0600

    Update

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+        <div className="">
+            <form>
+                <input type="text" />
+            </form>
+        </div>
+        );
+    }
+}
+
+export default SearchBar;
