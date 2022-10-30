7d16ce243658bf9fb5c64bd85a95594c128a9b7b
imsat <satyajit.cse@yahoo.com>
2022-10-30 17:01:21 +0600

    Update

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
+
+    state = { term: 'Hi there!', password: ''}
+
 
     render() {
         return (
@@ -11,8 +14,16 @@ class SearchBar extends React.Component {
             <form className="ui form">
                 <div className="field">
                 <label htmlFor="">Image Search</label>
-                <input type="text" onChange={this.onInputChange} />
+                {/* <input type="text" onChange={this.onInputChange} /> */}
+                <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value.toUpperCase()})} />
                 </div>
+
+                 {/* <div className="field"> */}
+                <label htmlFor="">Password</label>
+                {/* <input type="text" onChange={this.onInputChange} /> */}
+                <input type="password" value={this.state.password} onChange={(e) => this.setState({password: e.target.value})} />
+                {this.state.password.length < 4 ? 'Password must be at least 4 caracters' : ''}
+                {/* </div> */}
             </form>
         </div>
         );
