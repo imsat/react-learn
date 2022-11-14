d0503186ff304ee2c23b68357bb29208959b8a23
imsat <satyajit.cse@yahoo.com>
2022-11-14 19:21:20 +0600

    Update

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,17 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("Programming");
+  const [results, setResults] = useState([]);
+
+  console.log(results)
+
+  useEffect(() => {
+    const search =  async () => {
+      // https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&search=Programing&namespace=0&limit=10
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'opensearch', 
+          format: 'json',
+          search: term,
+          namespace: '0',
+          limit: '0',
+          origin: '*'
+        }
+      })
+
+      setResults(data)
+    }
+
+    search()
+
+    // (async () => {
+    //   await axios.get('')
+    // })();
+
+    // axios.get('')
+    // .then(res => {
+
+    // })
+
+  }, [term]);
+
   return (
     <div className="">
       <div className="ui form">
         <div className="field">
           <label htmlFor="">Enter Search Term</label>
-          <input 
-          type="text" 
-          className="input" 
-          value={term}  
-          onChange={e => {setTerm(e.target.value); console.log(e.target.value)}}
+          <input
+            type="text"
+            className="input"
+            value={term}
+            onChange={(e) => {
+              setTerm(e.target.value);
+            }}
           />
         </div>
       </div>
