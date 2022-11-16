7b7076db1da540372462f4b84eb24232ed20c378
imsat <satyajit.cse@yahoo.com>
2022-11-16 13:17:54 +0600

    Update

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "../Convert";
 
 const options = [
   { label: "Afrikaans", value: "af" },
@@ -30,6 +31,9 @@ const Translate = () => {
           onSelectedChange={setLanguage}
           options={options}
         />
+        <hr />
+        <h3 className="ui header">Output</h3>
+        <Convert language={language} text={text} />
       </div>
     </div>
   );
