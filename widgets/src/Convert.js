d66af3b4698f55544e7dfe45759b1021f672d845
imsat <satyajit.cse@yahoo.com>
2022-11-21 12:49:31 +0600

    Update

--- widgets/src/Convert.js
+++ widgets/src/Convert.js
@@ -1,27 +1,45 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
-const Convert = ({ language, text}) => {
-    const [translated, setTranslated] = useState('')
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
 
-    useEffect(() => {
-        const doTranslation = async () => {
-            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-                params: {
-                  q: text,
-                  target: language.value,
-                  key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'  
-                }
-              })
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
         }
-       
-    }, [language, text])
+      );
 
-    return (
-        <div className="">
-            rtr
-        </div>
-    )
-}
+      setTranslated(data.data.translations[0].translatedText);
+    };
 
-export default Convert
+    doTranslation();
+  }, [language, debouncedText]);
+
+  return (
+    <div className="">
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
+};
+
+export default Convert;
--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -6,6 +6,7 @@ const options = [
   { label: "Afrikaans", value: "af" },
   { label: "Arabic", value: "ar" },
   { label: "Hindi", value: "hi" },
+  { label: "Bangla", value: "bn" },
 ];
 
 const Translate = () => {
