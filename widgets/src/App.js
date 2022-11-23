3396a419264330f5ce2e479ec1a54cf5ba45c07b
imsat <satyajit.cse@yahoo.com>
2022-11-23 15:38:25 +0600

    Update

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -34,26 +36,33 @@ const options = [
   },
 ];
 
+
 export default () => {
-  // const [selected, setSelected] = useState(options[0]);
-  // const [showDropdown, setShowDropdown] = useState(true);
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Translate />
+      <Header />  
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
 
-      {/* <Search /> */}
-      {/* <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button> */}
-      {/* {showDropdown ? (
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
         <Dropdown
+          label="Select a color"
           selected={selected}
           onSelectedChange={setSelected}
           options={options}
         />
-      ) : null} */}
-      {/* <Accordion items={items} /> */}
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
