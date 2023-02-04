ea9f31f01704790de8a54782c4321be760d94251
imsat <satyajit.cse@yahoo.com>
2023-02-05 02:11:02 +0600

    Update

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,20 +1,31 @@
+import memoize from "lodash/memoize";
 import jsonPlaceholder from "../apis/jsonPlaceHolder";
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
-  dispatch({ 
-    type: "FETCH_POSTS", 
-    payload: response.data
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response.data,
   });
 };
 
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
-
-  dispatch({ 
-    type: "FETCH_USER", 
-    payload: response.data
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data,
   });
 };
+
+
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = memoize((id, dispatch) => {
+//   const response = jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+// });
