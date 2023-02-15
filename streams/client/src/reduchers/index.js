7d249867b565a9bfbc5b7f857e76cb36ccf7b5d0
imsat <satyajit.cse@yahoo.com>
2023-02-16 01:00:29 +0600

    Update

--- streams/client/src/reduchers/index.js
+++ streams/client/src/reduchers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from 'redux-form';
 
 import authReducher from "./authReducher";
 
 export default combineReducers({
-    auth: authReducher
+    auth: authReducher,
+    form: formReducer
 })
